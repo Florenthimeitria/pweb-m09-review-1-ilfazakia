ad7e912d5b4c3a82db4ba6e628b8a42921d63e17
ilfazakia <37678224+ilfazakia@users.noreply.github.com>
2018-04-07 15:03:03 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,5 +1,24 @@
 <?php
+  if ($_POST) 
+	{
+		$ipk = $_POST['ipk'];
+		if ($ipk<2.75) 
+		{
+		 	$keterangan ="<font color='red'>mahasiswa DO</font>";
+		}
+		else 
+		{
+			$keterangan ="mahasiswa tidak DO";
+		} 
 
+		echo "<br>";
+		echo "Status DO Mahasiswa <br>";
+		echo "IPK Mahasiswa : " . $ipk;
+		echo "<br>";
+		echo "Status : " . $keterangan;
+
+	}
+?>
 //TODO 5: Tentukan status mahasiswa berdasarkan input yang diberikan
 
 
